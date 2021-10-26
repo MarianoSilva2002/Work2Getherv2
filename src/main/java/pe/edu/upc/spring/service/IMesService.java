63f0a959879813056ef408cd3494cfdfaeeb149e
PhoenixLuxor <90794804+PhoenixLuxor@users.noreply.github.com>
2021-10-25 19:36:52 -0500

    Clase mes

---
+++ src/main/java/pe/edu/upc/spring/service/IMesService.java
@@ -0,0 +1,15 @@
+package pe.edu.upc.spring.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import pe.edu.upc.spring.model.Mes;
+
+public interface IMesService {
+	
+	public boolean grabar(Mes mes);
+	public void eliminar(int idMes);
+	public Optional<Mes> listarId(int idMes);
+	public List<Mes> listar();
+	public List<Mes> buscarNombre(String nombre);
+}
